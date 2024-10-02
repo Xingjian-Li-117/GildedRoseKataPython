d5aab310df14df53ec5bac7801584846eb321fb4
Xingjian-Li-117 <lixingjian_jane@hotmail.com>
2024-10-01 22:38:31 -0700

    Added 3 failed tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -10,6 +10,45 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+    
+    def test_aged_brie_quality(self):
+        brie = "Aged Brie"
+        items = [Item(brie, 2, 0), Item(brie, 1, 1)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.items[0].quality == 2
+        assert gr.items[1].quality == 0
+    
+    def test_sulfuras_quality_and_sell_in(self):
+        sulfuras = "Sulfuras, Hand of Ragnaros"
+        items = [Item(sulfuras, 0, 80), Item(sulfuras, -1, 80)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.items[0].quality == 81
+        assert gr.items[0].sell_in == 1
+        assert gr.items[1].quality == 81
+        assert gr.items[1].sell_in == 0
+    
+    def test_backstage_passes_quality(self):
+        backstage_pass = "Backstage passes to a TAFKAL80ETC concert"
+        items = [
+            Item(backstage_pass, 10, 20),
+            Item(backstage_pass, 5, 20),
+            Item(backstage_pass, 0, 20),
+        ]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        assert gr.items[0].quality == 23
+        assert gr.items[1].quality == 24
+        assert gr.items[2].quality == 1
+
+
 
 
 if __name__ == '__main__':
